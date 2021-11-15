7c826354466a0c36bb3131b755afd80147e4d091
Brandon Quach <bquach@umich.edu>
2021-11-15 17:49:34 -0500

    Activity start

--- pages/activity.js
+++ pages/activity.js
@@ -4,13 +4,13 @@ import { getAuth} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.j
 import "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBcDXz7O-3FkG2uCgTGWXY7Ay4aMyXE3N8",
-    authDomain: "chocolate-2c71d.firebaseapp.com",
-    projectId: "chocolate-2c71d",
-    storageBucket: "chocolate-2c71d.appspot.com",
-    messagingSenderId: "744292159993",
-    appId: "1:744292159993:web:866b078a19b17b2d21ad28",
-    measurementId: "G-3ZNXNEQ38Z"
+apiKey: "AIzaSyBcDXz7O-3FkG2uCgTGWXY7Ay4aMyXE3N8",
+authDomain: "chocolate-2c71d.firebaseapp.com",
+projectId: "chocolate-2c71d",
+storageBucket: "chocolate-2c71d.appspot.com",
+messagingSenderId: "744292159993",
+appId: "1:744292159993:web:866b078a19b17b2d21ad28",
+measurementId: "G-3ZNXNEQ38Z"
 };
 
 // Initialize Firebase
@@ -31,8 +31,49 @@ const auth = getAuth();
 var resultView = new Vue({
     el: '#activity',
     data: {
+      allTasks: [{'description' : "Go to CCRB0", location: "xyz0", "points": "0"}],
+      task1: 0,
+      task2: 1,
+      task3: 2,
+      selectedTask: -1,
     },
     methods: {
-
+      setTasks: function() {
+        for(let i = 1; i < 50; i++) {
+          resultView.allTasks.push(
+            {
+              description: "Go to CCRB" + i,
+              location: "xyz" + i,
+              points: i
+            }
+          )
+        }
+      },
+      getTasks: function() {
+        console.log("DAWD")
+        let counter = 0;
+        while(counter < 3) {
+          let num = Math.floor(Math.random() * 51);
+          if(counter == 0) {
+            resultView.task1 = num;
+            counter++;
+          } else if(counter == 1) {
+            if(resultView.task1 == num) {
+              console.log("")
+            } else {
+              resultView.task2 = num;
+              counter++;
+            }
+          } else if(counter == 2) {
+            if(resultView.task1 == num || resultView.task2 == num) {
+              console.log("");
+            } else {
+              resultView.task3 = num;
+              counter++
+            }
+          }
+        }
+      },
     }
 })
+resultView.setTasks();
